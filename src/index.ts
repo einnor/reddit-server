6556085c7bf5e212a964884ca4cee12403ca5cdb
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-08-21 18:10:14 +0300

    Server running on port 4000

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-import { Post } from './entities/Post';
 import mikroConfig from './mikro-orm.config';
+import express from 'express';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
 
   // Automatically run the migration
   orm.getMigrator().up();
-
-  // const post = orm.em.create(Post, { title: 'my first post' });
-  // await orm.em.persistAndFlush(post);
-
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  const app = express();
+  app.listen(4000, () => {
+    console.log('Server running on port 4000');
+  });
 };
 
 main().catch((err) => {
