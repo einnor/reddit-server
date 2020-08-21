b7fd5ab27166882dd15b3e24d19d69a771d3fb22
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-08-21 18:16:42 +0300

    Set up apollo server with testresolver

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import mikroConfig from './mikro-orm.config';
-import express from 'express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
@@ -9,6 +12,14 @@ const main = async () => {
   // Automatically run the migration
   orm.getMigrator().up();
   const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
   app.listen(4000, () => {
     console.log('Server running on port 4000');
   });
