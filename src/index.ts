3ca41f9502f54a2bf6c6c297217fe9c764ce9837
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-08-21 17:29:47 +0300

    Connection to the db

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
+import { Post } from './entities/Post';
 
 const main = async () => {
   const orm = await MikroORM.init({
+    entities: [Post],
     dbName: 'reddit',
     user: 'posgres',
     password: '',
     type: 'postgresql',
     debug: !__prod__,
   });
+
+  const post = orm.em.create(Post, { title: 'my first post' });
+  await orm.em.persistAndFlush(post);
 };
 
-main();
+main().catch((err) => {
+  console.log(err);
+});
