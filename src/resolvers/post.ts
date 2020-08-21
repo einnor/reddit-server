18b7272867235140880593ee6b2b455db8418026
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-08-21 18:37:59 +0300

    Resolver to get a post by id

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Ctx } from 'type-graphql';
+import { Resolver, Query, Ctx, Arg, Int } from 'type-graphql';
 import { Post } from '../entities/Post';
 import { MyContext } from 'src/types';
 
@@ -7,5 +7,13 @@ export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg('id', () => Int) id: number,
+    @Ctx() { em }: MyContext): Promise<Post | null>
+  {
+    return em.findOne(Post, { id });
   }
 }
